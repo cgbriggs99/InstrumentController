0ab8cc77972e6f70f028bcf618ba55b3efc545bb
Connor Briggs <cgbriggs99@gmail.com>
2024-06-27 23:09:41 -0400

    Added comments.

--- device-src/main/wifi_secrets.hpp
+++ device-src/main/wifi_secrets.hpp
@@ -2,9 +2,9 @@
 #define __WIFI_SECRETS__
 
 // Wifi information
-static const char *ssid = ;
-static const char *passwd = ;
-static const char *server = ;
-static uint16_t port = 1883;
+static const char *ssid = ; // Wifi name
+static const char *passwd = ; // Wifi password
+static const char *server = ; // IP address of MQTT broker
+static uint16_t port = 1883; // Port of MQTT broker. Default 1883
 
 #endif
